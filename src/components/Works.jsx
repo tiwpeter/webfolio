3fd542627b049737d2264adfb541733e26a0e068
tiwpeter <tiwpeter777@gmail.com>
2024-05-02 15:03:35 +0700

    Your commit message here

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,48 +1,57 @@
-import React from 'react'
+import React , {useState}from 'react'
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
 import { styles } from '../style'
-import { projects } from "../constants";
+import { projects } from "../constants"; // = id
 import {Tilt} from "react-tilt";
-
-const ProjectCard = ({
-  index,
-  name,
-  description,
-  tags,
-  image,
-  source_code_link,
-}) => {
-  return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
-      >
-        <div className="relative w-full h-[230px]">
-          <img
-            src={image}
-            alt={name}
-            className='w-full h-full object-cover rounded-2xl'
-          />
-        </div>
-
-        <div className="mt-5">
-          <h3>{name}</h3>
-          <p>{description}</p>
-        </div>
-      </Tilt>
-    </motion.div>
-  );
-}
+import ProjectTag from './ProjectTag'
 
 function Works() {
+  const [tag, setTag] = useState("All"); // ย้าย useState ไปใน Works()
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag)
+  };
+
+  const ProjectCard = ({
+    index,
+    name,
+    description,
+    tags,
+    image,
+    source_code_link,
+  }) => {
+    return (
+      <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+        <Tilt
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        >
+          <div className="relative w-full h-[230px]">
+            <img
+              src={image}
+              alt={name}
+              className='w-full h-full object-cover rounded-2xl'
+            />
+          </div>
+
+          <div className="mt-5">
+            <h3>{name}</h3>
+            <p>{description}</p>
+          </div>
+        </Tilt>
+      </motion.div>
+    );
+  }
+
   return (
     <>
+      <h2 className=''>d</h2>
+
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>
           My work
@@ -50,23 +59,23 @@ function Works() {
         <h2 className={styles.sectionHeadText}>
           Project
         </h2>
+        <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+          <ProjectTag 
+          onClick={handleTagChange} 
+          tag="All" 
+          isSelected={tag === "All"}
+          />
+
+          <ProjectTag 
+          onClick={handleTagChange} 
+          tag="Web" 
+          isSelected={tag === "Web"}
+          />
+        </div>
       </motion.div>
 
-      <div className="w-full flex">
-      <motion.p
- variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-      >
-         Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
-      </motion.p>
-      </div>
-      
       <div className="mt-20 flex flex-wrap gap-7">
-      {projects.map((project, index) => (
+        {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -201,7 +201,7 @@ const projects = [
     name: "Car Rent",
     description:
       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-    tags: [
+    tags: [ "All","Web",
       {
         name: "react",
         color: "blue-text-gradient",
