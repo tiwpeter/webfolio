bce7cc62b6c20a28294e366979b90ed2e3704109
tiwpeter <tiwpeter777@gmail.com>
2024-05-03 16:22:49 +0700

    Your commit message here

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,6 +5,7 @@ import { styles } from '../style';
 import { projects } from "../constants"; 
 import { Tilt } from "react-tilt";
 import ProjectTag from './ProjectTag';
+import { github } from "../assets";
 
 function Works() {
   const [type, setType] = useState("All"); 
@@ -18,7 +19,7 @@ function Works() {
     project.tags.some(tag => tag.name === type)
   );
 
-  const ProjectCard = ({ index, name, description, image }) => {
+  const ProjectCard = ({ index, name, description, image,source_code_link,tags }) => {
     return (
       <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
         <Tilt
@@ -36,10 +37,34 @@ function Works() {
               className='w-full h-full object-cover rounded-2xl'
             />
           </div>
-          <div className="mt-5">
-            <h3>{name}</h3>
-            <p>{description}</p>
+
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
           </div>
+
+          <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
         </Tilt>
       </motion.div>
     );
